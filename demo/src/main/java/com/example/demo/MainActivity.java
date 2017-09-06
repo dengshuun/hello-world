cf9d2e5cbd0e0bb1ccfff2de90bdd724bb514a59
dengshun <dengshun1025@gmail.com>
2017-09-06 10:48:59 +0800

    2017/9/6

--- demo/src/main/java/com/example/demo/MainActivity.java
+++ demo/src/main/java/com/example/demo/MainActivity.java
@@ -1,18 +1,33 @@
 package com.example.demo;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private IrregularImageView ivBlowUP;
+    private IrregularImageView ivBlowFront;
+    private IrregularImageView ivBlowDown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ivBlowUP = (IrregularImageView) findViewById(R.id.iv_blower_up);
+        ivBlowUP.setOnClickListener(this);
+        ivBlowFront = (IrregularImageView) findViewById(R.id.iv_blower_front);
+        ivBlowFront.setOnClickListener(this);
+        ivBlowDown = (IrregularImageView) findViewById(R.id.iv_blower_down);
+        ivBlowDown.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v.getVisibility() == View.VISIBLE)
+            v.setVisibility(View.INVISIBLE);
+        else if (v.getVisibility() == View.INVISIBLE)
+            v.setVisibility(View.VISIBLE);
+    }
 }
